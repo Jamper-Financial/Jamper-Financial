345154a402546cf91bf8dcd7a2460a4b7533cf8c
JohnPaolo18 <Paolokahlil@gmail.com>
2024-11-25 01:36:21 -0700

    Log in and Create Account + database

---
+++ Jamper/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,9 @@
+﻿using Foundation;
+
+namespace Jamper;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
