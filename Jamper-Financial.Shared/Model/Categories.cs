5fa1306faa0dc3b3d0e1bf36c65031611f466519
Prayus <prayus.sharma@edu.sait.ca>
2025-02-11 01:06:04 -0700

    Manage categories created
    added categories to the project and database and a handler for managing categories

---
+++ Jamper-Financial.Shared/Model/Categories.cs
@@ -0,0 +1,10 @@
+﻿namespace Jamper_Financial.Shared.Models 
+{
+    public class Category
+    {
+        public int CategoryID { get; set; }
+        public int UserID { get; set; } 
+        public string Name { get; set; } = string.Empty;
+    }
+
+}
