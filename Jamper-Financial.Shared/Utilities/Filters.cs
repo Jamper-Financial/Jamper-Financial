edaebdf106eb164e24d637483ae27506a1146e8d
Emil Cabutotan <113380544+emildave@users.noreply.github.com>
2025-03-31 23:38:58 -0600

    removed filter
    removed all of the filter code from calendar page, will work on the filtering after sort.
    I also moved the filter modal/dropdown to its own component page for reusability.

--- Jamper-Financial.Shared/Utilities/Filters.cs
+++ Jamper-Financial.Shared/Utilities/Filters.cs
@@ -1,19 +1,17 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Jamper_Financial.Shared.Utilities
 {
     public static class Filters
     {
-        public static List<Transaction> ApplyFilters(List<Transaction> transactions, string filterCategory, double? minAmount, double? maxAmount)
+        public static List<Transaction> ApplyFilters(List<Transaction> transactions, string filterCategory, decimal? minAmount, decimal? maxAmount)
         {
             return transactions
                 .Where(t => (string.IsNullOrEmpty(filterCategory) || t.CategoryID.ToString().Equals(filterCategory, StringComparison.OrdinalIgnoreCase)) &&
-                            (!minAmount.HasValue || (minAmount.Value >= 0 ? (t.Amount >= (decimal)minAmount || t.Amount >= (decimal)minAmount) : (t.Amount >= (decimal)minAmount || t.Amount >= (decimal)minAmount))) &&
-                            (!maxAmount.HasValue || (maxAmount.Value >= 0 ? (t.Amount <= (decimal)maxAmount || t.Amount <= (decimal)maxAmount) : (t.Amount <= (decimal)maxAmount || t.Amount <= (decimal)maxAmount))))
+                            (!minAmount.HasValue || t.Amount >= minAmount) &&
+                            (!maxAmount.HasValue || t.Amount <= maxAmount))
                 .ToList();
         }
     }
